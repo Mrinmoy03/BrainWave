47357aa69c58b3ba51bad005f40e311e70c63a08
Mrinmoy Bhattacharjee <98102401+Mrinmoy03@users.noreply.github.com>
2025-03-27 01:41:02 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ButtonGreadient from "./assets/svg/ButtonGradient";
+
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGreadient />
+    </>
+  );
+};
+
+export default App;
+
+// 4:40
