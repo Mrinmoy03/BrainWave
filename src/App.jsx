628567e23ff62f0e8892f2a55bbd945481af0267
Mrinmoy Bhattacharjee <98102401+Mrinmoy03@users.noreply.github.com>
2025-03-27 16:29:00 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ButtonGreadient from "./assets/svg/ButtonGradient";
 
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGreadient />
     </>
@@ -15,5 +17,4 @@ const App = () => {
 };
 
 export default App;
-
-// 4:40
+// 5:18
